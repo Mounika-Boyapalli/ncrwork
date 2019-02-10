9c5680cad440ccf925f9575302e980d9091b1003
Mounika-Boyapalli <47222387+Mounika-Boyapalli@users.noreply.github.com>
2019-02-10 12:46:50 +0530

    Added string reverse program

---
+++ revString.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+
+
+void reverse(char *);
+
+
+
+int main()
+
+{
+    
+    char s[50];
+
+    printf("\nEnter the string");
+
+    scanf("%[^\n]s",s);
+
+    reverse(s);
+
+    return 0;
+
+}
+
+
+
+void reverse(char *a)
+
+{
+
+    if(*a)
+
+    {
+
+        reverse(a+1);
+
+        printf("%c",*a);
+
+    }
+
+}
