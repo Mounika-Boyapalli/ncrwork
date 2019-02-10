6edf59062e00027d5f40649bbfeeb61210f9bca4
Mounika-Boyapalli <47222387+Mounika-Boyapalli@users.noreply.github.com>
2019-02-10 16:01:32 +0530

    Added strtok() program

---
+++ strtokFun.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+#include <string.h>
+
+
+int main()
+
+{
+
+    char s[50];
+
+    printf("\nEnter the string");
+
+    scanf("%s",s);
+
+    char *a= strtok(s,".");
+
+    while(a!=NULL)
+
+    {
+
+        printf("%s\n", a);
+ 
+       a = strtok(NULL, ".");
+ 
+    }
+ 
+    getch();
+
+    return 0;
+
+}
