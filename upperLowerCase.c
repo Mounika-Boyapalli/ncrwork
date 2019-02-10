28e44de1c37ab143fd1d83a16c431051e7f987e8
Mounika-Boyapalli <47222387+Mounika-Boyapalli@users.noreply.github.com>
2019-02-10 11:50:33 +0530

    Adding lower-upper and upper-lower case conversion

---
+++ upperLowerCase.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+
+int main()
+
+{
+
+    char s[50];
+
+    int i, l;
+
+    printf("\nEnter the string");
+
+    scanf_s("%[^\n]s",s);
+
+    for(l=0;s[l];l++);
+
+    for(i=0;i<l;i++)
+
+    {
+
+        if((s[i]>='A')&&(s[i]<='Z'))
+
+        {
+
+            s[i] = s[i] + 32;
+
+        }
+
+        else if((s[i]>='a')&&(s[i]<='z'))
+
+        {
+
+            s[i] = s[i] - 32;
+   
+      }
+
+    }
+
+    printf("%s",s);
+
+ 
+
+   getch();
+    return 0;
+
+}
