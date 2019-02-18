08ab9ab7dc61d6d6919c5aa155c5612e94bbeaf0
mounikareddy2697@gmail.com <mounikareddy2697@gmail.com>
2019-02-18 22:40:30 +0530

    Assignment 1 q7 (C++)

--- matrix_mul/matrix_mul/matrix_mul.cpp
+++ matrix_mul/matrix_mul/matrix_mul.cpp
@@ -1,39 +1,68 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
-class mul
+class matrix
 {
-	int m;
-	int n;
-public: mul (int x)
-        {
-			m = x;
-			n = x;
-	    }
-		void get_mat(mul o)
-		{
-			int i;
-		}
-};
+	int a[3][3];
 
+public:
+	void get_data()
+	{
+
+		int i, j;
+		for (i = 0; i < 3; i++)
+		{
+			for (j = 0; j < 3; j++)
+				cin >> a[i][j];
+
+		}
+
+	}
+	friend matrix mult(matrix, matrix);
+	void display()
+	{
+		int i, j;
+		for (i = 0; i < 3; i++)
+		{
+			for (j = 0; j < 3; j++)
+				cout << a[i][j] << " ";
+			cout << endl;
+
+		}
+	}
+};
+matrix mult(matrix m1, matrix m2)
+{
+	matrix m;
+	int k, i, j;
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 3; j++)
+		{
+			m.a[i][j] = 0;
+		}
+
+	}
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 3; j++)
+		{
+			for (k = 0; k < 3; k++)
+			{
+				m.a[i][j] = m.a[i][j] + m1.a[i][k] + m2.a[k][i];
+
+			}
+		}
+	}
+	return m;
+
+}
 int main()
 {
-	int a,i,j;
-	cout << "Enter the number of rows and columns";
-	cin >> a ;
-	mul m1(a), m2(a);
-	int **ip;
-	ip = new int*[a];
-	for (i = 0; i < a; i++)
-	{
-		ip[i] = new int[a];
-	}
-	cout << "Enter the elements";
-	for (i = 0; i < a; i++)
-	{
-		for (j = 0; j < a; j++)
-		{
-
-		}
-	}
-	return 0;
+	matrix m1, m2, m3;
+	m1.get_data();
+	m2.get_data();
+	m3 = mult(m1, m2);
+	m3.display();
+	system("pause");
+	return 0; s
 }
