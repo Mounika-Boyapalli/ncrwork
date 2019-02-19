6e1f3dd64e073bace4040f18c367d71334fa9b29
mounikareddy2697@gmail.com <mounikareddy2697@gmail.com>
2019-02-19 21:28:27 +0530

    Postfix Evaluation

--- ds_postfix_eval/ds_postfix_eval/ds_postfix_eval.cpp
+++ ds_postfix_eval/ds_postfix_eval/ds_postfix_eval.cpp
@@ -17,6 +17,7 @@ public: _stack_();
 		bool IsFull();
 		~_stack_();
 		void getsize(int);
+		int convert(char);
 };
 
 _stack_::_stack_()
@@ -73,26 +74,77 @@ _stack_::~_stack_()
 {
 	delete sta.s;
 }
-
+int _stack_::convert(char g)
+{
+	switch (g)
+	{
+	case '0': return 0;
+		break;
+	case '1': return 1;
+		break;
+	case '2': return 2;
+		break;
+	case '3': return 3;
+		break;
+	case '4': return 4;
+		break;
+	case '5': return 5;
+		break;
+	case '6': return 6;
+		break;
+	case '7': return 7;
+		break;
+	case '8': return 8;
+		break;
+	case '9': return 9;
+		break;
+	}
+}
 int main()
 {
 	_stack_ s1;
-	int siz, l, i;
-	char ch, *str;
-	cout << "Enter the size of the stack" << endl;
+	int l, siz, i, v, result, h;
+	char ch, *sp;
+	cout << "Enter the size of stack" << endl;
 	cin >> siz;
-	s1.getsize(siz);
 	cout << "Enter the length of the expression" << endl;
 	cin >> l;
-	str = new char[l];
-	cout << "Enter the expression" << endl;
-	cin >> str;
-	for (i = 0; str[i]; i++)
+	sp = new char[l];
+	cout << "Enter the expression";
+	cin >> sp;
+	s1.getsize(siz);
+	for (i = 0; sp[i]; i++)
 	{
-		ch = (int)str[i];
-		str[i] = ch;
+		ch = sp[i];
+		if (isdigit(ch))
+		{
+			v = s1.convert(ch);
+			s1.push(v);
+		}
+		else
+		{
+			switch (ch)
+			{
+			case '+': h = s1.pop();
+				result = s1.pop() + h;
+				s1.push(result);
+				break;
+			case '-': h = s1.pop();
+				result = s1.pop() - h;
+				s1.push(result);
+				break;
+			case '*': h = s1.pop();
+				result = s1.pop() * h;
+				s1.push(result);
+				break;
+			case '/': h = s1.pop();
+				result = s1.pop() / h;
+				s1.push(result);
+				break;
+			}
+		}
 	}
-
+	cout << "Result is " << s1.pop() << endl;
 	system("pause");
 	return 0;
 }
